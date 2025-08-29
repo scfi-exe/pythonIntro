206373283db91657b9ce9aa25d036ac896665224
scfi-odoo <scfi@odoo.com>
2025-08-28 21:30:10 -0400

    phone formatter

---
+++ 28. Phone Number Formatter Exercise.py
@@ -0,0 +1,32 @@
+# 📱 Phone Number Formatter
+#
+# 1. Ask the user to enter a U.S. phone number in **any format**.
+# 2. Use .strip() to remove any leading/trailing spaces.
+# 3. Replace common separators (-, (, ), .) with spaces.
+# 4. Use .split() to break into chunks, then .join() to merge the digits.
+# 5. Check if the cleaned number has **exactly 10 digits**.
+# 6. If yes, format it like this: (123) 456-7890
+# 7. If not, print an error message: "Please enter exactly 10 digits."
+
+while True:
+    number = input("Please enter in a phone number to format: ")
+
+    # number = number.replace("-", "")
+    # number = number.replace(",", "")
+    # number = number.replace(".", "")
+    # number = number.replace(" ", "")
+    # number = number.replace("+", "")
+
+    for ch in ["-", " ", "(", ")", "+", "."]:
+        number = number.replace(ch, "")
+
+    if len(number) == 10:
+        print(f"({number[:3]}) {number[3:6]}-{number[6:]}")
+        # number = number[:0] + "(" + number[0:]
+        # number = number[:4] + ")" + number[4:]
+        # number = number[:5] + " " + number[5:]
+        # number = number[:9] + "-" + number[9:]
+        # print(number)
+        break
+    else:
+        print("ERROR: Please enter exactly 10 digits.")
