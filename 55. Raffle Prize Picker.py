808d94cf1b99a2f954969d39ba2d8fb1a0f74406
scfi-exe <scottfinkelstein97@gmail.com>
2025-08-31 12:39:49 -0400

    Raffle Prize Picker completed

--- 55. Raffle Prize Picker.py
+++ 55. Raffle Prize Picker.py
@@ -5,22 +5,13 @@ import random
 # 1. Ask how many people are entering the raffle (at least 3 names). [x]
 # 2. Use a loop to collect their names into a list. [x]
 # 3. Ask for exactly 3 prize names (in order) and store them in a list. [x]
-# 4. Randomly pick 3 different winners from the participant list.
-# 5. Print out who wins which prize and make sure the final one
+# 4. Randomly pick 3 different winners from the participant list. [x]
+# 5. Print out who wins which prize and make sure the final one [x]
 #    is clearly marked as the Grand Prize. 🏆
 #
 # Hint: Use loops, lists, and a tool that picks random items without repeats. (i think this is random.sample(xxx))
 
 participants = []
-
-# for i in range(3):
-#     name = input(
-#         f"Thanks for entering the raffle! You are participant number {i+1}. What is your name?: "
-#     )
-#     participants.append(name)
-
-# # separator.join(iterable) --> ", ".join(participants)
-# print(f"The current raffle participants are: {", ".join(participants)}")
 
 while True:
 
@@ -36,19 +27,21 @@ while True:
     else:
         participants.append(name)
 
-print(f"The current raffle participants are: {", ".join(participants)}")
-print(f"There are currently {len(participants)} participants in the raffle.")
+# print(f"The current raffle participants are: {", ".join(participants)}") #sanity check
+# print(f"There are currently {len(participants)} participants in the raffle.") #sanity check
 
 prizeList = []
 for i in range(3):
     prize = input(f"What will prize #{i+1} be?: ")
     prizeList.append(prize)
 
-print(prizeList)
+# print(prizeList) #sanity check
 
-# randomly pick 3 winners
-for i in range(3):
-    winner = participants[(random.sample(participants))]
+# create a new list, winners, by using random.sample() to grab 3 UNIQUE participants
+winners = random.sample(participants, 3)
+
+# enumerate over the winners list -> we are able to do "i, winner" because of "enumerate(winner)", otherwise we would get an error
+for i, winner in enumerate(winners):
     if i == 0:
         print(f"{i+1}. {winner} has won the grand prize: {prizeList[i]}!")
     else:
