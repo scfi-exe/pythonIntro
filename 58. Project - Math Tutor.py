40c732b1e7eedb1af2c0b1eac2571b66ab788e7a
scfi-exe <scottfinkelstein97@gmail.com>
2025-09-01 09:56:59 -0400

    updated math tutor

--- 58. Project - Math Tutor.py
+++ 58. Project - Math Tutor.py
@@ -1,23 +1,40 @@
 # import modules
 from random import randrange as r
+import time
 
 # ask how many questions user wants
-numQuestions = int(input("How many questions do you want?"))
+numQuestions = int(input("How many questions do you want?: "))
+# bonus #2: allow the user to specify how high the numbers used should be
+lowerRange = int(input("What should the lower range of numbers be?: "))
+upperRange = int(input("What should the upper range of numbers be?: "))
 # set score start at zero
 playerScore = 0
+totalTime = 0.0
+
 # loop through number of questions
 for question in range(numQuestions):
     # create two random numbers and calc answer
-    numA = r(1, 10)
-    numB = r(1, 10)
+    numA = r(lowerRange, upperRange)
+    numB = r(lowerRange, upperRange)
     correctAnswer = numA * numB
     # show user the question
+    # bonus #1: capture time between user inputs
+    startTime = time.perf_counter()
     userAnswer = int(input(f"What is {numA} * {numB} = "))
+    endTime = time.perf_counter()
+    timeTaken = round((endTime - startTime), 2)
+    totalTime = totalTime + timeTaken
     # capture answer and modify user score
     if userAnswer == correctAnswer:
         playerScore = playerScore + 1
-        print("You are correct! Time for your next question...\n")
+        print(
+            f"You are correct! You took {timeTaken} seconds to answer. Time for your next question...\n"
+        )
     else:
-        print("Sorry, that answer is incorrect. Better luck next time!\n")
+        print(
+            f"Sorry, that answer is incorrect. The correct answer was {correctAnswer}. Better luck next time!\n"
+        )
 # output final score
-print(f"Your final score is: {playerScore} out of {numQuestions} correct!")
+print(
+    f"Your final score is: {playerScore} out of {numQuestions} correct!\nYou took {totalTime} seconds to answer all {numQuestions} questions."
+)
